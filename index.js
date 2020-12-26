44f295c6039d163c1003bd8f71267413820c515c
mess <erald.muhammad@gmail.com>
2020-12-26 15:32:30 +0700

    feat: new page

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-$(document).ready(function(){
-  $(document.getElementById("headerDropupButton")).click(function(){
-    $(document.getElementById("headerDropup")).slideToggle('slow', 'swing');
+$(document).ready(function () {
+  $("#headerDropupButton").click(function () {
+    $(document.getElementById("headerDropup")).slideToggle("slow", "swing");
+  });
+
+  $(".items").slick({
+    variableWidth: true,
+    dots: true,
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
 
-function closeHeader(){
-  $(document.getElementById("headerDropup")).slideToggle('fast', 'linear');
+function closeHeader() {
+  $(document.getElementById("headerDropup")).slideToggle("fast", "linear");
 }
